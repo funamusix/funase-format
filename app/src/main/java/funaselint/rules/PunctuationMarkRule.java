3ff993577419275e9339b257efb493eda058f6fc
wshuhei <w.shuhei0127@icloud.com>
2023-11-18 00:30:50 +0900

    Update PunctuationMarkRule.java

--- app/src/main/java/funaselint/rules/PunctuationMarkRule.java
+++ app/src/main/java/funaselint/rules/PunctuationMarkRule.java
@@ -1,11 +1,6 @@
 package funaselint.rules;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.w3c.dom.Document;
