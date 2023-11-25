c8073c84478a9062475544e39dd192c6f83f2738
wshuhei <w.shuhei0127@icloud.com>
2023-11-25 02:46:43 +0000

    feat:テキストのフォントをチェックする機能を実装

---
+++ app/src/main/java/funaselint/rules/TextFontRule.java
@@ -0,0 +1,57 @@
+package funaselint.rules;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class TextFontRule extends Rule {
+
+    @Override
+    public List<Path> applicablePath() {
+        return List.of(Paths.get("ppt/slides/"));
+    }
+
+    @Override
+    public List<RuleApplicationResult> applyRule(Document doc, Path filePath, boolean fixEnabled) {
+        List<RuleApplicationResult> results = new ArrayList<>();
+
+        // フォント情報を取得
+        NodeList textFontNodes = doc.getElementsByTagName("a:latin");
+
+        for (int i = 0; i < textFontNodes.getLength(); i++) {
+            Element textFontElement = (Element) textFontNodes.item(i);
+
+            // フォント名を取得
+            String fontName = textFontElement.getAttribute("typeface");
+
+            // "UD"で始まるフォントに対する警告
+            if (fontName.startsWith("UD")) {
+                results.add(new RuleApplicationResult(this, filePath, false));
+                break;
+            }
+
+            // "MS ゴシック"に対する警告
+            if (fontName.equals("ＭＳ ゴシック")) {
+                results.add(new RuleApplicationResult(this, filePath, false));
+                break;
+            }
+        }
+
+        return results;
+    }
+
+    @Override
+    public String getMessage() {
+        return "特定のフォントは使用しないでください";
+    }
+
+    @Override
+    public String getFunaseMessage() {
+        return "あんまりこうUフォントやMSゴシックって嫌いなのよ";
+    }
+}
